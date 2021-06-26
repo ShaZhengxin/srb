50c5594c874ba33b1ae636551fc707ae18c07fa0
ShaZhengxin <szx0829@mail.ustc.edu.cn>
2021-06-26 20:37:34 +0800

    尚融宝项目接口工程

---
+++ service-core/src/main/java/com/szx/srb/core/ServiceCoreApplication.java
@@ -0,0 +1,19 @@
+package com.szx.srb.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @Description TODO
+ * @Author 沙政鑫
+ * @Data 2021/6/26  20:23
+ */
+
+@SpringBootApplication
+@ComponentScan({"com.szx.srb"})//使范围大些，可以访问其他微服务包
+public class ServiceCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCoreApplication.class, args);
+    }
+}
---
+++ service-core/src/main/java/com/szx/srb/core/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.szx.srb.core.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Description TODO
+ * @Author 沙政鑫
+ * @Data 2021/6/26  20:27
+ */
+@Configuration
+@MapperScan("com.szx.srb.core.mapper")
+@EnableTransactionManagement//事务处理
+public class MybatisPlusConfig {
+
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//分页
+        return interceptor;
+    }
+}
